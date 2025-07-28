c1027c60e7c4a484f90e6e3ad882881a77993467
Shawn-le-don-epic <shawnfranco.christy@gmail.com>
2025-07-28 13:31:51 +0530

    Changed the rate limit in upstash.js

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -5,7 +5,7 @@ dotenv.config();
 // ratelimiter for 100 requestes per 60 seconds
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(10, "20 s")
+    limiter: Ratelimit.slidingWindow(100, "60 s")
 });
 
 
