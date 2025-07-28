5ad393e66849164f767dea06dea4b046ad855298
Shawn-le-don-epic <shawnfranco.christy@gmail.com>
2025-07-28 13:12:32 +0530

    Prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import cors from "cors";
 import dotenv from "dotenv"
 import express from "express"
+import path from "path";
 
 //local imports
 import notesRoutes from "../routes/notesRoutes.js";
@@ -13,6 +14,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve(); //to get the current directory name
 
 
 
@@ -21,10 +23,14 @@ const PORT = process.env.PORT || 5001
 //cors middleware must preceed rateLimiter because the latter needs to send a status code
 //that is not allowed by default in CORS
 //if rateLimiter is used after cors, the response will not be sent to the client
-app.use(cors({
-    origin: 'http://localhost:5173',
-}
-)); //CORS middleware to allow cross-origin requests
+if(process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: 'http://localhost:5173',
+        }
+    )); //CORS middleware to allow cross-origin requests
+}    
+
 app.use(express.json()); //midleware parses JSON bodies, giving access to req.body
 app.use(rateLimiter);
 
@@ -35,6 +41,14 @@ app.use(rateLimiter);
 // })
 
 app.use("/api/notes", notesRoutes);
+
+if( process.env.NODE_ENV === "production" ) {
+    app.use(express.static(path.join(__dirname, "../frontend/dist"))); //serving static files from frontend/dist
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    }); //to serve the index.html file for all other routes
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
